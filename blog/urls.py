8f49a2aec2e9bc75c9bc9897b00f9bd3822531e5
abdelhalimresu <abdelhalim.resu@gmail.com>
2022-12-23 10:58:38 +0100

    Add view post

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from blog.views import add_post
+from blog.views import add_post, view_posts
 
 urlpatterns = [
-    path('post/add', add_post)
+    path('post/add', add_post),
+    path('posts/', view_posts)
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 
 from blog.forms import PostForm
+from blog.models import Post
 
 # Create your views here.
+@login_required(login_url='/admin/')
 def add_post(request):
     if request.method == 'POST':
         post = PostForm(request.POST)
@@ -11,9 +15,21 @@ def add_post(request):
             post = post.save(commit=False)
             post.author = request.user.profile
             post.save()
-            return HttpResponse('OK')
+            return redirect('/posts')
         else:
             return HttpResponse('ERROR')
     else:
         form = PostForm()
         return render(request, 'post_add.html', {'form': form})
+
+
+def view_posts(request):
+    search = request.GET.get('search')
+    # get all posts from the database: post = Post.objects...
+    if search:
+        posts = Post.objects.filter(
+            Q(title__contains=search) | Q(text__contains=search)
+            ).order_by('-created_date')
+    else:
+        posts = Post.objects.all().order_by('-created_date')
+    return render(request, 'view_posts.html', {'posts': posts, 'title': 'All Posts'})
